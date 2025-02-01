e9de0c83f7f1e40386d27f85d0272c1c76a8199f
정우창 <woochang4862@gmail.com>
2025-02-01 17:40:11 +0900

    아두이노 포트 자동 감지 기능 추가

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 from serial import *
+from serial.tools import list_ports
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 import time
 from datetime import datetime
 from threading import Thread
-import os
-
-# Firebase Admin SDK 초기화
-cred = credentials.Certificate('serviceAccountKey.json')
-firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://support-764f8-default-rtdb.firebaseio.com/'
-})
 
 last_value = 0
+
+def find_arduino_port():
+    arduino_ports = [
+        p.device
+        for p in list_ports.comports()
+        if 'ttyACM' in p.description  # 아두이노 설명이 포함된 포트만 선택
+    ]
+    
+    if not arduino_ports:
+        raise Exception("아두이노를 찾을 수 없습니다.")
+        
+    return arduino_ports[0]
 
 def get_data(ser):
     global last_value
@@ -29,9 +35,21 @@ def get_data(ser):
     
             
 if __name__ == '__main__':
-    ser = Serial('/dev/ttyACM0', 9600, timeout=1)
-    ref = db.reference('laundry')
+    try:
+        # Firebase Admin SDK 초기화
+        cred = credentials.Certificate('serviceAccountKey.json')
+        firebase_admin.initialize_app(cred, {
+            'databaseURL': 'https://support-764f8-default-rtdb.firebaseio.com/'
+        })    
+    except Exception as e:
+        print(f'firebase connection error : {e}')
     
+    try:
+        arduino_port = find_arduino_port()
+        ser = Serial(arduino_port+"@", 9600, timeout=1)
+    except Exception as e:
+        print(f'arduino connection error : {e}')
+        
     thread1 = Thread(target = get_data, args=(ser,))
     thread1.start()
     while True:
@@ -42,13 +60,14 @@ if __name__ == '__main__':
             20 초 
             15 초 9 와트 ***
             """
+            ref = db.reference('laundry')
             current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             ref.set(last_value >= 8)
             print(f"저장됨: {current_time} - {last_value}")
             time.sleep(15)
             
         except Exception as e:
-            print(f'firebase error : {e}')
+            print(f'error : {e}')
     
     ser.close()
     
