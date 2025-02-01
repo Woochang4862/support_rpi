517cce09ab6d18524819a0cda59d6cdffcedc886
Woochang4862 <woochang4862@gmail.com>
2025-02-01 17:36:09 +0900

    fix : test_find_arduino_port.py

--- test/test_find_arduino_port.py
+++ test/test_find_arduino_port.py
@@ -2,11 +2,10 @@ from serial import *
 from serial.tools import list_ports
 
 def find_arduino_port():
-    """아두이노 포트를 자동으로 찾는 함수 (고급 버전)"""
     arduino_ports = [
         p.device
         for p in list_ports.comports()
-        if 'Arduino' in p.description  # 아두이노 설명이 포함된 포트만 선택
+        if 'ttyACM' in p.description  # 아두이노 설명이 포함된 포트만 선택
     ]
     
     if not arduino_ports:
